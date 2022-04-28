a285fd14bd2c89446c1c5d44215edcdaabc3a2ca
DimitriDovlev <dimitri.dovlev2000@gmail.com>
2022-04-28 20:38:21 +0200

    step 4 in progress

--- Workshop_class_9/API/middleware/role.validation.js
+++ Workshop_class_9/API/middleware/role.validation.js
@@ -1,5 +1,19 @@
 const Joi = require("joi");
 
-const userSchema=Joi.object({
-    
-})
+const userSchema = Joi.object({});
+
+// Validation for user
+const userValidation = (req, res, next) => {
+  const userData = req.body;
+  console.log(req.headers);
+
+  next();
+};
+
+//
+const adminValidation = (req, res, next) => {
+  const userData = req.body;
+  console.log(req);
+};
+
+module.exports = userValidation;
--- Workshop_class_9/API/routes/dish.routes.js
+++ Workshop_class_9/API/routes/dish.routes.js
@@ -2,13 +2,17 @@ const router = require("express").Router();
 const DishController = require("../controllers/dish.controller");
 const sessionValidator = require("../middleware/session.validation");
 const tokenValidation = require("../middleware/token.validation");
+const roleValidation = require("../middleware/role.validation");
 
 // middleware to check if the user is logged in to use the routes
 // router.use(sessionValidator);
-router.use(tokenValidation);
+
+// router.use(tokenValidation);
+// router.use(roleValidation);
+
 // 1. Get all dish items from the DB
 // http://localhost:5000/dish/
-router.get("/", DishController.getAllDishes);
+router.get("/", roleValidation, DishController.getAllDishes);
 // 2. Get dish by id
 // http://localhost:5000/dish/:id
 router.get("/:id", DishController.getDishById);
